685164a108f4a639b6def743c03f48578ef79472
TimurSamigulin <47005114+TimurSamigulin@users.noreply.github.com>
2021-11-28 21:39:16 +0300

    update readme

--- README.md
+++ README.md
@@ -5,3 +5,17 @@
 https://github.com/mozilla/geckodriver/releases
 
 Установить зависимости: pip install -r requirements.txt
+
+- parsing 
+
+Парсинг ссылок с Яндекс и Гугл по определенным поисковым запросам.
+Все данные спаршены уже и хранятся по пути data/model/uritexts, нет 
+смысла снова парсить их снова. Но чтобы запустить данный модуль, 
+необходимо запустить файл main.py в корневой папке проекта.
+
+- processing
+
+Модуль извлечение информации из текстов, для его работы запустите 
+файл scripts/processing/main_processing.py
+
+Результаты модуля processing хранятся в data/result
--- scripts/processing/main_processing.py
+++ scripts/processing/main_processing.py
@@ -7,32 +7,32 @@ from scripts.processing import processing
 
 def get_en_org_info():
     input_file = '../../data/model/uritexts/Крупнейшие мировые производители силового кабеля/texts.csv'
-    output_file = '../../data/model/uritexts/Крупнейшие мировые производители силового кабеля/en_org_info.csv'
+    output_file = '../../data/result/en_org_info.csv'
     processing.get_en_orgs_info(input_file, output_file)
 
 
 def get_ru_org_info():
     input_file = '../../data/model/uritexts/Крупнейшие мировые производители силового кабеля/texts.csv'
-    output_file = '../../data/model/uritexts/Крупнейшие мировые производители силового кабеля/ru_org_info1.csv'
+    output_file = '../../data/result/ru_org_info.csv'
     processing.get_ru_orgs_info(input_file, output_file)
 
 
 def get_gost():
-    output_file = '../../data/model/uritexts/Кабель определение/kabel_gost.txt'
+    output_file = '../../data/result/kabel_gost.txt'
     processing.get_gost_info(output_file)
 
 
 def get_kabel_class():
-    output_file = '../../data/model/uritexts/Классификация кабелей/kabel_class.txt'
+    output_file = '../../data/result/kabel_class.txt'
     processing.get_kabel_class(output_file)
 
 def get_share_prices():
-    output_dir = Path('..', '..', 'data', 'model', 'uritexts', 'Прогноз цены на силовой кабель')
+    output_dir = Path('..', '..', 'data', 'result')
     processing.get_share_prices(output_dir)
 
 if __name__ == '__main__':
-    # get_en_org_info()
+    get_en_org_info()
     get_ru_org_info()
-    # get_gost()
-    # get_kabel_class()
-    # get_share_prices()
+    get_gost()
+    get_kabel_class()
+    get_share_prices()
